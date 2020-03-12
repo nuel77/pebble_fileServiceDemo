e2b8ce963c4b792ad65c959532ce5613bf5ef713
Emmanuel Thomas Devasia <50878033+emmanuel010@users.noreply.github.com>
2020-03-12 10:51:35 +0530

    new downloads library

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.post('/download',(req,res)=>{
     res.set('Content-disposition', 'attachment; filename=' + currentState["filename"]);
     res.set('Content-Type', currentState["contentType"]);
     res.append('name',currentState["filename"] );
-
+    res.append('content',currentState["contentType"])
     //console.log(res)
     fileContents.on('end',()=>res.end())
     fileContents.pipe(res)
